944dd8ee0f97973e026f8d7db20ddead31404a9e
vchandak12 <vchandak172@gmail.com>
2024-06-25 13:45:40 +0530

    atm

---
+++ atmp.java
@@ -0,0 +1,39 @@
+package atm;
+
+public class atmp {
+	
+	    private double balance;
+	    private double depositAmount;
+	    private double withdrawAmount;
+
+	    //default constructor
+	    public atmp(){
+
+	    }
+
+	    //getter setter
+	public double getBalance() {
+	        return balance;
+	    }
+
+	    public void setBalance(double balance) {
+	        this.balance = balance;
+	    }
+
+	    public double getDepositAmount() {
+	        return depositAmount;
+	    }
+	    public void setDepositAmount(double depositAmount) {
+	        this.depositAmount = depositAmount;
+	    }
+
+	    public double getWithdrawAmount() {
+	        return withdrawAmount;
+	    }
+
+	    public void setWithdrawAmount(double withdrawAmount) {
+	        this.withdrawAmount = withdrawAmount;
+	    }
+	
+  
+}
